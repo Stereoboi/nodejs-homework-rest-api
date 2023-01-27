73c7707580bc7140f7392b85a4efd0aadb5dfc1b
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-01-27 21:04:47 +0200

    master

--- app.js
+++ app.js
@@ -24,9 +24,6 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/", (req, res) => {
-  res.send("<h1>CONTACT_DB</h1>");
-});
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", authRouter);
 
