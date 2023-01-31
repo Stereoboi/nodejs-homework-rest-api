3c542737582e7a288d1da7e99990ee7cc9116ca4
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-01-31 16:04:27 +0200

    master

--- controllers/contactsControllers/updateContactController.js
+++ controllers/contactsControllers/updateContactController.js
@@ -3,8 +3,8 @@ const contacts = require("../../models/contacts");
 const updateContactController = async (req, res) => {
   const { contactId } = req.params;
   const { _id } = req.user;
-  await contacts.updateContact(contactId, req.body, _id);
-  res.status(200).json({ status: "Successfully updated", id: `${contactId}` });
+  const contact = await contacts.updateContact(contactId, req.body, _id);
+  res.status(200).json(contact);
 };
 
 module.exports = updateContactController;
