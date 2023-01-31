6ae1bcc44dcd43d6dd4a0509c853d0cccc93f9af
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-01-31 15:12:28 +0200

    master

--- controllers/contactsControllers/deleteContactController.js
+++ controllers/contactsControllers/deleteContactController.js
@@ -4,7 +4,10 @@ const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const { _id } = req.user;
   await contacts.removeContact(contactId, _id);
-  res.status(200).json({ message: `contact id:${contactId} deleted` });
+  res.status(200).json({
+    message: `contact id:${contactId} deleted`,
+    id: `${contactId}`,
+  });
 };
 
 module.exports = deleteContactController;
