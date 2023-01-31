15686e1fc84d5e0de2ca2fa49514bebf7bd3dfc8
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-01-31 14:29:10 +0200

    master

--- controllers/contactsControllers/addNewContactController.js
+++ controllers/contactsControllers/addNewContactController.js
@@ -3,15 +3,15 @@ const contacts = require("../../models/contacts");
 const addNewContactController = async (req, res) => {
   const { name, email, phone, favorite = false } = req.body;
   const { _id } = req.user;
-  await contacts.addContact({ name, email, phone, favorite }, _id);
+  const contact = await contacts.addContact(
+    { name, email, phone, favorite },
+    _id
+  );
   res.status(201).json(
     { status: "Successfully created" },
     {
       contact: {
-        name: { name },
-        email: { email },
-        phone: { phone },
-        _id: { _id },
+        contact,
       },
     }
   );
